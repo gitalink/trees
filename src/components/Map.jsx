b76982e2cd8fa3f709c383c86b8e043c0e52e17a
Gita Linkeviciute <gita.link@gmail.com>
2020-06-30 12:32:46 -0400

    Change to using hooks

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import mapboxgl, { Marker } from 'mapbox-gl';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ2l0YWxpbmsiLCJh IjoiY2thbHJsNWloMTNmYzJ5bW54M3ZhY3pycyJ9.6AEc1-JrLZHIYMwmuzTtQg';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2l0YWxpbmsiLCJhIjoiY2thbHJsNWloMTNmYzJ5bW54M3ZhY3pycyJ9.6AEc1-JrLZHIYMwmuzTtQg';
 
 export default () => {
   const [state, setState] = useState({
     lat: 40.748360,
-    lng: -73.985402, 
+    lng: -73.985402,
     zoom: 10
   });
 
@@ -21,12 +21,12 @@ export default () => {
     });
 
     map.on('load', () => {
-      fetch(`https://data.cityofnewyork.us/resource /5rq2-4hqu.json?zip_city=New%20York&$select=tree_id,%20spc_common,%20the_geom,%20spc_latin&$limit =100000`)
+      fetch(`https://data.cityofnewyork.us/resource/5rq2-4hqu.json?zip_city=New%20York&$select=tree_id,%20spc_common,%20the_geom,%20spc_latin&$limit=1000`)
         .then(res => res.json())
         .then(json => {
-          json.forEach((t) => {
+          json.forEach((tree) => {
             const m = new Marker()
-              .setLngLat(t.the_geom.coordinates)
+              .setLngLat(tree.the_geom.coordinates)
               .addTo(map)
           });
         });
@@ -35,37 +35,37 @@ export default () => {
 
   return (
     <div>
-      <div ref={el=>mapContainer=el} className="map Container" />
+      <div ref={el=>mapContainer=el} className="mapContainer" />
     </div>
   );
 };
 
-class Map extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 2
-    }
-  }
+// class Map extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       lng: 5,
+//       lat: 34,
+//       zoom: 2
+//     }
+//   }
 
-  componentDidMount() {
-    const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom
-    })
-  }
+//   componentDidMount() {
+//     const map = new mapboxgl.Map({
+//       container: this.mapContainer,
+//       style: 'mapbox://styles/mapbox/streets-v11',
+//       center: [this.state.lng, this.state.lat],
+//       zoom: this.state.zoom
+//     })
+//   }
 
-  render() {
-    return (
-      <div>
-        <div ref={el=>this.mapContainer=el} className="mapContainer" />
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <div ref={el=>this.mapContainer=el} className="mapContainer" />
+//       </div>
+//     )
+//   }
+// }
 
 // export default Map
