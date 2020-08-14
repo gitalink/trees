ae59bba7fa7f7e2388003127c1f8fbd721ea6fc7
Gita Linkeviciute <gita.link@gmail.com>
2020-08-14 17:41:18 -0400

    Add tree tiles layer to the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,7 +7,7 @@ export default () => {
   const [state, setState] = useState({
     lat: 40.748360,
     lng: -73.985402,
-    zoom: 10
+    zoom: 9
   });
 
   let mapContainer;
@@ -20,21 +20,55 @@ export default () => {
       zoom: state.zoom
     });
 
+    // map.on('load', function() {
+    //   map.addSource('trees-tiles', )
+    // })
+
     map.on('load', () => {
-      fetch(`https://data.cityofnewyork.us/resource/5rq2-4hqu.json?zip_city=New%20York&$select=tree_id,%20spc_common,%20the_geom,%20spc_latin&$limit=1000`)
-        .then(res => res.json())
-        .then(json => {
-          json.forEach((tree) => {
-            const m = new Marker()
-              .setLngLat(tree.the_geom.coordinates)
-              .addTo(map)
-          });
-        });
+      map.addSource('trees', {
+        type: 'vector',
+        url: 'mapbox://gitalink.trees-tiles'
+      })
+
+      map.addLayer({
+        'id': 'trees-id',
+        'type': 'circle',
+        'source': 'trees',
+        'source-layer': 'trees',
+        'paint': {
+          'circle-radius': 7,
+          'circle-color': '#ff69b4'
+        }
+      })
+
+      map.addLayer({
+        'id': 'trees-label',
+        'type': 'symbol',
+        'source': 'trees',
+        'source-layer': 'trees',
+        'layout': {
+          'text-field': ['get', 'spc_common'],
+        'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+        'text-radial-offset': 0.5,
+        'text-justify': 'auto'
+        }
+      })
+
+    //   fetch(`https://data.cityofnewyork.us/resource/5rq2-4hqu.json?zip_city=New%20York&$select=tree_id,%20spc_common,%20the_geom,%20spc_latin&$limit=1000`)
+    //     .then(res => res.json())
+    //     .then(json => {
+    //       json.forEach((tree) => {
+    //         const m = new Marker()
+    //           .setLngLat(tree.the_geom.coordinates)
+    //           .addTo(map)
+    //       });
+    //     });
     });
   }, [state]);
 
   return (
     <div>
+      <h1>New York City Trees Mapped</h1>
       <div ref={el=>mapContainer=el} className="mapContainer" />
     </div>
   );
